e980f47766f632827a53bba9fb40e8542700f476
Ndamulelo <ndamulel.mulaudzi@gmail.com>
2024-01-15 11:30:04 +0200

    updated responsiveness

--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react'
-import { useNavigate} from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import { PRODUCTS } from '../../Products'
 import { ShopContext } from '../../context/ShopContext'
 import { CartItem } from './CartItem'
@@ -8,8 +8,8 @@ import './Cart.css'
 
 
 
-export  function Cart() {
-    const {cartItems, getTotalAmount} = useContext(ShopContext)
+export function Cart() {
+    const { cartItems, getTotalAmount } = useContext(ShopContext)
     const totalAmount = getTotalAmount()
     const navigate = useNavigate()
     return (
@@ -19,19 +19,19 @@ export  function Cart() {
             </div>
             <div className="cartItems">
                 {PRODUCTS.map((product) => {
-                    if(cartItems[product.id] !==0){
+                    if (cartItems[product.id] !== 0) {
                         return <CartItem data={product} />
                     }
                 })}
             </div>
-            {totalAmount >0 ?
+            {totalAmount > 0 ?
                 <div className="checkout">
-                    
-                    <p>Subtotal : R {totalAmount}</p>
-                    <button onClick={(() => navigate("/") )}>Continue Shopping</button>
+
+                    <p>Subtotal : R {Math.round(totalAmount)}</p>
+                    <button onClick={(() => navigate("/"))}>Continue Shopping</button>
                     <button>Check Out</button>
                 </div>
-            : <h1>Your Cart is empty</h1> }
+                : <h1>Your Cart is empty</h1>}
         </div>
     )
 }
